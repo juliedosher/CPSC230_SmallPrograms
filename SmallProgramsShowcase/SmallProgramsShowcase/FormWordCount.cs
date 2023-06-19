8de4fc673afff767a59703d0ea7d98337600c2fb
juliedosher <juliegdosher@lewisu.edu>
2023-06-19 18:30:46 -0500

    completed word counter

--- SmallProgramsShowcase/SmallProgramsShowcase/FormWordCount.cs
+++ SmallProgramsShowcase/SmallProgramsShowcase/FormWordCount.cs
@@ -16,7 +16,66 @@ namespace SmallProgramsShowcase
         {
             InitializeComponent();
 
+            btnCount.Click += BtnCount_Click;
             btnClose.Click += BtnClose_Click;
+        }
+
+        private void BtnCount_Click(object sender, EventArgs e)
+        {
+            rtbCount.Text = "";
+            Char[] delimiters = { ' ', '\n', '\r', ',', '.', ':', ';' };
+            string[] words = rtbStory.Text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToLower();
+            }
+
+            if (words.Length > 0)
+            {
+                Dictionary<string, int> wordCount = GetWordCount(words);
+                wordCount = AlphabetizeDictionary(wordCount);
+                foreach (var word in wordCount)
+                {
+                    rtbCount.Text += word.Key + ": " + word.Value + "\n";
+                }
+        }
+            else
+            {
+                MessageBox.Show("Please enter a story first.");
+            }
+        }
+
+        private Dictionary<string, int> GetWordCount(string[] words)
+        {
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (wordCount.ContainsKey(words[i]))
+                {
+                    wordCount[words[i]]++;
+                }
+
+                else
+                {
+                    wordCount.Add(words[i], 1);
+                }
+            }
+            return wordCount;
+        }
+
+        private static Dictionary<string, int> AlphabetizeDictionary(Dictionary<string, int> originalDict)
+        {
+            var orderedWords = new Dictionary<string, int>();
+
+            var list = originalDict.Keys.ToList();                                                  
+            list.Sort();
+
+            foreach (var word in list)
+            {
+                orderedWords.Add(word, originalDict[word]);
+            }
+
+            return orderedWords;
         }
 
         private void BtnClose_Click(object sender, EventArgs e)
