a9818424f068689f65e05971b636847dc14dd8e1
juliedosher <juliegdosher@lewisu.edu>
2023-06-18 23:33:06 -0500

    started word counter

--- SmallProgramsShowcase/SmallProgramsShowcase/FormBMI.Designer.cs
+++ SmallProgramsShowcase/SmallProgramsShowcase/FormBMI.Designer.cs
@@ -45,6 +45,7 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(2, 1);
             this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(288, 97);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "Enter your weight in pounds and height in inches below. Then click Calculate.";
