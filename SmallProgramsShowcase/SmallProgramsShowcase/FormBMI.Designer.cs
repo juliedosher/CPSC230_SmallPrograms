a65da4d190ec27609fe9f157c8d9846f0ff049df
juliedosher <juliegdosher@lewisu.edu>
2023-06-18 23:24:55 -0500

    created BMI form

--- SmallProgramsShowcase/SmallProgramsShowcase/FormBMI.Designer.cs
+++ SmallProgramsShowcase/SmallProgramsShowcase/FormBMI.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbWeight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbHeight = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.rtbBMI = new System.Windows.Forms.RichTextBox();
+            this.rtbStatus = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(2, 1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(288, 97);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Enter your weight in pounds and height in inches below. Then click Calculate.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Weight (lb)";
+            // 
+            // tbWeight
+            // 
+            this.tbWeight.Location = new System.Drawing.Point(86, 117);
+            this.tbWeight.Name = "tbWeight";
+            this.tbWeight.Size = new System.Drawing.Size(100, 20);
+            this.tbWeight.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Height (in)";
+            // 
+            // tbHeight
+            // 
+            this.tbHeight.Location = new System.Drawing.Point(86, 157);
+            this.tbHeight.Name = "tbHeight";
+            this.tbHeight.Size = new System.Drawing.Size(100, 20);
+            this.tbHeight.TabIndex = 4;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(208, 135);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 5;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Your BMI is ...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Your status is ...";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(111, 339);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // rtbBMI
+            // 
+            this.rtbBMI.BackColor = System.Drawing.SystemColors.Control;
+            this.rtbBMI.Location = new System.Drawing.Point(111, 220);
+            this.rtbBMI.Name = "rtbBMI";
+            this.rtbBMI.Size = new System.Drawing.Size(44, 21);
+            this.rtbBMI.TabIndex = 9;
+            this.rtbBMI.Text = "";
+            // 
+            // rtbStatus
+            // 
+            this.rtbStatus.BackColor = System.Drawing.SystemColors.Control;
+            this.rtbStatus.Location = new System.Drawing.Point(111, 259);
+            this.rtbStatus.Name = "rtbStatus";
+            this.rtbStatus.Size = new System.Drawing.Size(112, 21);
+            this.rtbStatus.TabIndex = 10;
+            this.rtbStatus.Text = "";
+            // 
+            // FormBMI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.ClientSize = new System.Drawing.Size(295, 374);
+            this.Controls.Add(this.rtbStatus);
+            this.Controls.Add(this.rtbBMI);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.tbHeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbWeight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "FormBMI";
+            this.Text = "BMI Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbWeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbHeight;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.RichTextBox rtbBMI;
+        private System.Windows.Forms.RichTextBox rtbStatus;
     }
 }
